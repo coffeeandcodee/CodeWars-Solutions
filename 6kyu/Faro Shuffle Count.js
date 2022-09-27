df0e798b49771cca14c8b269af94368c37d3e392
coffeeandcodee <87856165+coffeeandcodee@users.noreply.github.com>
2022-09-27 16:16:38 +0100

    Create Faro Shuffle Count.js

---
+++ 6kyu/Faro Shuffle Count.js
@@ -0,0 +1,8 @@
+function faroCount(deckSize){
+  let shuffles = 0, term = 1;
+  do {
+    shuffles++;
+    term = (deckSize * (term % 2) + term) / 2 | 0; 
+  } while (term !== 1)
+  return shuffles;
+}
